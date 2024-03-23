4a5fa92c6462b54631a8ade900cb25f5487c63e6
Gabriela Guevara <agabrielaguevaraa@hotmail.com>
2024-03-23 13:31:19 -0500

    Adelanto de sabado 23 marzo clases 9 y 10

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,6 +1,18 @@
+window.addEventListener('load', iniciarJuego)
+
+let ataqueJugador
+let ataqueEnemigo
+
 function iniciarJuego(){
     let botonMascotaJugador = document.getElementById('boton-mascota')
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+    
+    let botonFuego = document.getElementById("boton-fuego")
+    botonFuego.addEventListener('click', ataqueFuego)
+    let botonAgua = document.getElementById("boton-Agua")
+    botonAgua.addEventListener('click', ataqueAgua)
+    let botonTierra = document.getElementById("boton-Tierra")
+    botonTierra.addEventListener('click', ataqueTierra)
 }
 
 function seleccionarMascotaJugador(){
@@ -8,19 +20,24 @@ function seleccionarMascotaJugador(){
     let spanMascotaJugador = document.getElementById("nomMascotaJugador")
 
     const mascotaSeleccionada = document.querySelector('input[name="mascota"]:checked')
-    if (mascotaSeleccionada){
-        alert("La mascota seleccionada es: " + mascotaSeleccionada.id)
-        spanMascotaJugador.innerHTML = mascotaSeleccionada.id
-    }else{
-        alert("Debes seleccionar una opción")
-    }
 
+    if (!mascotaSeleccionada) {
+        alert("Debes seleccionar una mascota antes de continuar.");
+        return;
+    }
+    
+    alert("La mascota seleccionada es: " + mascotaSeleccionada.id)
+    spanMascotaJugador.innerHTML = mascotaSeleccionada.id
+    
+    
     seleccionarMascotaEnemigo()
 }
 
+
+
 function seleccionarMascotaEnemigo(){
 
-    let spanMascotaEnemigo = document.getElementById("nomMascotaJugador")
+    let spanMascotaEnemigo = document.getElementById("nomMascotaEnemigo")
 
     const diccionarioOpciones={
         "hipodogue":"Hipodogue","capipepo":"Capipepo","ratigueya":"Ratigueya","langostelvis":"Langostelvis", "tucapalma":"Tucapalma",
@@ -31,13 +48,69 @@ function seleccionarMascotaEnemigo(){
     const resultadoAleatorio = diccionarioOpciones[Object.keys(diccionarioOpciones)[indiceAleatorio]];
 
     alert("La mascota del enemigo es: " + resultadoAleatorio)
-    spanMascotaEnemigo.innerHTML = mascotaSeleccionada.id
+    spanMascotaEnemigo.innerHTML = resultadoAleatorio
 
 }
 
-function aleatorio(min, max){
-    return Math.floor(Math.random() * (max - min + 1 ) + 1)
+function ataqueFuego(){
+
+    const mascotaSeleccionada = document.querySelector('input[name="mascota"]:checked')
+
+    if (!mascotaSeleccionada) {
+        alert("Debes seleccionar una mascota antes de continuar.");
+        return;
+    }
+
+    ataqueJugador = "Fuego"
+    ataqueAleatorioEnemigo()
 }
 
+function ataqueAgua(){
 
-window.addEventListener('load', iniciarJuego)
+    const mascotaSeleccionada = document.querySelector('input[name="mascota"]:checked')
+
+    if (!mascotaSeleccionada) {
+        alert("Debes seleccionar una mascota antes de continuar.");
+        return;
+    }
+
+    ataqueJugador = "Agua"
+    ataqueAleatorioEnemigo()
+}
+
+function ataqueTierra(){
+
+    const mascotaSeleccionada = document.querySelector('input[name="mascota"]:checked')
+
+    if (!mascotaSeleccionada) {
+        alert("Debes seleccionar una mascota antes de continuar.");
+        return;
+    }
+
+    ataqueJugador = "Tierra"
+    ataqueAleatorioEnemigo()
+}
+
+function crearMensaje(){
+    let sectionMensajes = document.getElementById("mensajes")
+
+    let parrafo = document.createElement("p")
+    parrafo.innerHTML = "Tu máscota atacó con " + ataqueJugador + ", la mascota del enemigo atacó con " + ataqueEnemigo +  " Pendiente"
+
+    sectionMensajes.appendChild(parrafo)
+}
+
+function ataqueAleatorioEnemigo(){
+    
+    const diccionarioAtaques={
+        "fuego":"Fuego","agua":"Agua","tierra":"Tierra"
+    }
+
+    const ataqueAleatorio = Math.floor(Math.random() * Object.keys(diccionarioAtaques).length);
+    const enemigoAleatorio = diccionarioAtaques[Object.keys(diccionarioAtaques)[ataqueAleatorio]];
+
+    ataqueEnemigo = enemigoAleatorio
+    //alert("El ataque del enemigo es: " + enemigoAleatorio)
+    crearMensaje()
+}
+
