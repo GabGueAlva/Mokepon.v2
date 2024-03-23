b812701e1f63fda40b958ee93b4ccdfce98ed6e7
Gabriela Guevara <agabrielaguevaraa@hotmail.com>
2024-03-22 20:54:49 -0500

    Adelanto hasta la clase 6 viernes 22 marzo

--- js/mokepon.js
+++ js/mokepon.js
@@ -14,6 +14,30 @@ function seleccionarMascotaJugador(){
     }else{
         alert("Debes seleccionar una opción")
     }
+
+    seleccionarMascotaEnemigo()
 }
+
+function seleccionarMascotaEnemigo(){
+
+    let spanMascotaEnemigo = document.getElementById("nomMascotaJugador")
+
+    const diccionarioOpciones={
+        "hipodogue":"Hipodogue","capipepo":"Capipepo","ratigueya":"Ratigueya","langostelvis":"Langostelvis", "tucapalma":"Tucapalma",
+        "pydos":"Pydos"
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * Object.keys(diccionarioOpciones).length);
+    const resultadoAleatorio = diccionarioOpciones[Object.keys(diccionarioOpciones)[indiceAleatorio]];
+
+    alert("La mascota del enemigo es: " + resultadoAleatorio)
+    spanMascotaEnemigo.innerHTML = mascotaSeleccionada.id
+
+}
+
+function aleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1 ) + 1)
+}
+
 
 window.addEventListener('load', iniciarJuego)
